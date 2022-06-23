6f9b65f19e49653b2e435728de61b0b282c9563e
Vinay Kalia <vinaykalia@google.com>
2022-06-23 22:29:16 +0000

    bigocean: Fix MIF floor for heavier loads
    For 10-bit content at max resolutio and fps, the effective
    load is more than the max defined load. Fix the MIF floor
    for such heavy load usecases to prevent framedrops.
    
    bug: 233707844
    bug: 237706931
    
    Signed-off-by: Vinay Kalia <vinaykalia@google.com>
    Change-Id: I6582546fdefd5da356a9b3b5c2ae9f4e860663ec
    (cherry picked from commit 71f6ca8d99bb9b573359e91b435ba34a1288cbce)

--- bigo_priv.h
+++ bigo_priv.h
@@ -54,6 +54,7 @@ struct bigo_bw {
 struct power_manager {
 	int bwindex;
 	struct exynos_pm_qos_request qos_bigo;
+	struct exynos_pm_qos_request qos_req_mif;
 	struct list_head opps;
 	struct list_head bw;
 	u32 max_load;
@@ -131,6 +132,8 @@ struct bigo_inst {
 	u32 hw_cycles[AVG_CNT];
 	struct completion job_comp;
 	struct bigo_job job;
+	/* bytes per pixel */
+	u32 bpp;
 };
 
 inline void set_curr_inst(struct bigo_core *core, struct bigo_inst *inst);
