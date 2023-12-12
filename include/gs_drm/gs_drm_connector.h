ef997631c1b62070896e3deab756be8c845b77d0
Taylor Nelms <tknelms@google.com>
2023-12-12 14:21:29 +0000

    drm: add change for tout_gpio compatibility
    Bug: 297994688
    Test: None
    Change-Id: I505af5fe9c030a782d515e2f46b72e85d2c728db
    Signed-off-by: Taylor Nelms <tknelms@google.com>

--- include/gs_drm/gs_drm_connector.h
+++ include/gs_drm/gs_drm_connector.h
@@ -104,6 +104,12 @@ struct gs_drm_connector_state {
 	 */
 	int te_gpio;
 
+	/*
+	 * @tout_gpio: Provides the gpio for panel TOUT (TE2) signal.
+	 *	       This is used for checking panel refresh rate.
+	 */
+	int tout_gpio;
+
 	/**
 	 * @partial: Specify whether this panel supports partial update feature.
 	 */
