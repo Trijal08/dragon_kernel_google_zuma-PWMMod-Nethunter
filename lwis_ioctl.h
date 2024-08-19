187a9d78e9214f90a5b00fde636d352f4da27efb
Luis Useche <useche@google.com>
2024-08-19 23:49:17 +0000

    Revert "Revert "Decouple the handling of different transaction c..."
    Revert submission 2912450-revert-2862839-dma_fences-SJLGWCAWZV
    
    Reason for revert: The user space code is almost ready to roll. We should check in this code again.
    
    Reverted changes: /q/submissionid:2912450-revert-2862839-dma_fences-SJLGWCAWZV
    
    Bug: 316416863
    Change-Id: I6b894848c0f7c84ec96266431dee83d2ad65823b

--- lwis_ioctl.h
+++ lwis_ioctl.h
@@ -13,6 +13,19 @@
 
 #include "lwis_device.h"
 
+/* Operations to handle different versions of the transaction commands.  */
+struct cmd_transaction_submit_ops {
+	/* Size of the command coming in from user space. */
+	size_t cmd_size;
+	/* Function to populate LWIS kernel transaction from user space command. */
+	void (*populate_transaction_info_from_cmd)(void *cmd, struct lwis_transaction *transaction);
+	/* Function to populte the user space command from LWIS kernel transaction
+         * before returning it up. The error has the error value returned from
+         * submitting the transaction. */
+	void (*populate_cmd_info_from_transaction)(void *cmd, struct lwis_transaction *transaction,
+						   int error);
+};
+
 /*
  *  lwis_ioctl_handler: Handle all IOCTL commands via the file descriptor.
  */
