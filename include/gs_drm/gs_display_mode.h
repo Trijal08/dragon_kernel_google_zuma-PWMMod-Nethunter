5cfc6642ed9c994d35e7bd114b1e5e48fe2d8214
attis <attis@google.com>
2024-08-23 20:48:46 +0800

    gs_panel: update drm clock checking macro.
    Improve readability of drm clock value checking macro.
    
    Bug: 354658706
    Test: Build with/without wrong drm clock settings.
    Change-Id: I3dddc66883e6068554ac5cbd506681164fa7e7d0
    Signed-off-by: attis <attis@google.com>

--- include/gs_drm/gs_display_mode.h
+++ include/gs_drm/gs_display_mode.h
@@ -66,6 +66,23 @@
 	.vtotal = VDISPLAY + VFP + VSA + VBP
 
 /**
+ * CALC_DRM_CLOCK_HZ() - calculates drm clock value
+ * @REFRESH_FREQ: Image refresh frequency, in Hz
+ * @HDISPLAY: Horizontal active region
+ * @HFP: Horizontal front porch
+ * @HSA: Horizontal sync
+ * @HBP: Horizontal back porch
+ * @VDISPLAY: Vertical active region
+ * @VFP: Vertical front porch
+ * @VSA: Vertical sync
+ * @VBP: Vertical back porch
+ *
+ * This macro calculated the pixel clock for the struct drm_display_mode.
+ */
+#define CALC_DRM_CLOCK_HZ(REFRESH_FREQ, HDISPLAY, HFP, HSA, HBP, VDISPLAY, VFP, VSA, VBP) \
+	((REFRESH_FREQ) * (HDISPLAY + HFP + HSA + HBP) * (VDISPLAY + VFP + VSA + VBP))
+
+/**
  * CHECK_DRM_CLOCK_DIVISIBLE_1000() - Checks drm clock value
  * @CLOCK: drm clock rate
  *
@@ -74,6 +91,15 @@
  */
 #define CHECK_DRM_CLOCK_DIVISIBLE_1000(CLOCK) (0 * sizeof( \
 	struct {static_assert((CLOCK) % 1000 == 0, "drm clock not divisible by 1000."); }))
+
+/**
+ * CALC_DRM_CLOCK_K_HZ()
+ * @CLOCK: drm clock rate
+ *
+ * This macro checks drm clock value is divisible by 1000, and returns (drm clock value/1000).
+ */
+#define CALC_DRM_CLOCK_K_HZ(CLOCK) \
+	(CHECK_DRM_CLOCK_DIVISIBLE_1000(CLOCK) + (CLOCK / 1000))
 
 /**
  * DRM_MODE_TIMING() - fills in timing parameters in struct drm_display_mode
@@ -89,17 +115,16 @@
  *
  * This macro calculates the pixel clock for use in the struct drm_display_mode
  * structure as well as the horizontal and vertical timing parameters (by way
- * of the DRM_H_TIMING() and DRM_V_TIMING() macros). Further checks the clock value
- * being divisible by 1000 through macro CHECK_DRM_CLOCK_DIVISIBLE_1000().
+ * of the DRM_H_TIMING() and DRM_V_TIMING() macros). Further checks and sets the
+ * clock value being divisible by 1000 through macro CALC_DRM_CLOCK_K_HZ().
  *
  * Context: This macro asserts for no fractional refresh rates. Please double-check the
  *          resulting .clock member against a known target value, especially for lower
  *          framerates otherwise may cause compilation error!
  */
 #define DRM_MODE_TIMING(REFRESH_FREQ, HDISPLAY, HFP, HSA, HBP, VDISPLAY, VFP, VSA, VBP) \
-	.clock = (HDISPLAY + HFP + HSA + HBP) * (VDISPLAY + VFP + VSA + VBP) * REFRESH_FREQ / 1000 \
-	+ CHECK_DRM_CLOCK_DIVISIBLE_1000( \
-		(HDISPLAY + HFP + HSA + HBP) * (VDISPLAY + VFP + VSA + VBP) * REFRESH_FREQ), \
+	.clock = CALC_DRM_CLOCK_K_HZ( \
+		CALC_DRM_CLOCK_HZ(REFRESH_FREQ, HDISPLAY, HFP, HSA, HBP, VDISPLAY, VFP, VSA, VBP)), \
 	DRM_H_TIMING(HDISPLAY, HFP, HSA, HBP), \
 	DRM_V_TIMING(VDISPLAY, VFP, VSA, VBP)
 
